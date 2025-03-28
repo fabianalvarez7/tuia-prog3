173e6ece61722eb3bf9b8e78ca3c1ff3984859d2
FabricioPorcelli <fabricioporcelli@gmail.com>
2025-03-27 22:55:59 -0300

    desarrollo algoritmo bfs

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -16,7 +16,7 @@ class BreadthFirstSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node("", grid.start, 0, parent=None, action=None)
 
         # Initialize the explored dictionary to be empty
         explored = {} 
@@ -24,4 +24,47 @@ class BreadthFirstSearch:
         # Add the node to the explored dictionary
         explored[node.state] = True
         
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
+
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a queue
+        frontier = QueueFrontier()
+        frontier.add(node)
+
+        # do
+        while True:
+
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.remove()
+
+            successors = grid.get_neighbours(node.state)
+
+        #     forall a in problema.acciones(n.estado) do
+            for action in successors:
+                new_state = successors[action]
+                
+                # Check if the successor is not reached
+                if new_state not in explored:
+
+                    # Initialize the son node
+                    new_node = Node("", new_state,
+                                    node.cost + grid.get_cost(new_state),
+                                    parent=node, action=action)
+
+                    # Mark the successor as reached
+                    explored[new_state] = True
+
+                    # Return if the node contains a goal state
+                    # In this example, the goal test is run
+                    # before adding a new node to the frontier
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+
+                    # Add the new node to the frontier
+                    frontier.add(new_node)
+
         return NoSolution(explored)
